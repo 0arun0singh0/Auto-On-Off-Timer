db4cf8be650b33ffba6f9a4dca2ad58382d515b8
0arun0singh0 <32683608+0arun0singh0@users.noreply.github.com>
2018-03-28 18:54:21 +0530

    Add files via upload

---
+++ timer_beta.c
@@ -0,0 +1,81 @@
+void main() {
+
+
+TRISB=1;
+TRISD=0;
+ ANSELB=0;
+ LATD=0b10000000;
+
+while(1)                            //infinite loop
+{
+
+
+
+//timer
+if(PORTB.B3==1)
+{
+timer:
+delay_ms(1000);                      //check for longer timer
+if(PORTB.B3==1)                    //if button still pressed after 1s
+{
+LATD=0b00110000;                     //2 LEDs on
+delay_ms(5400000);             //1.5 hr  timer
+LATD=0b10000000;
+ }
+ else
+ {
+LATD=0b00010000;                  //single LED
+delay_ms(3600000);                  //1 hr timer
+LATD=0b10000000;
+ }  }
+else
+
+
+
+
+
+ //Permanent on
+ if(PORTB.B1==1)
+ {
+
+delay_ms(500);
+while(1)
+{
+LATD=0b01000000;
+if(PORTB.B2==1)
+{
+LATD=0b10000000;
+break;
+ }
+ else
+if(PORTB.B3==1)
+{
+
+goto timer;               //back to timer
+
+ } }  }
+
+
+ else
+
+
+
+
+
+// Permanent off
+if(PORTB.B2==1)
+ {
+ delay_ms(500);
+LATD=0b10000000;
+}
+
+
+
+
+
+
+
+ }
+
+
+}
